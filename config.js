983cb5e79703d6fe8327eddad401e84f8faa44fe
khadoanh <87489239+khadoanh@users.noreply.github.com>
2021-07-15 23:12:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
     introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Nếu em đang xem cái này thì anh đang...`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    title: 'Chúc mừng sinh nhật tuổi 19 của em 😙',
+    desc: 'Em có vui khi thấy nó ',
+    btnYes: 'ko, em ko vui <33',
+    btnNo: 'Có, em rất vui :3',
+    question:'Qua tuổi mới em mong muốn điều gì nhất <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    reply: 'Em vẫn cute trong mắt anh <33333333',
+    mess: 'Sinh nhật vui vẻ 🥰. bé iu của anh 😘😘',
+    messDesc: 'Hết dịch 2 đứa cùng đi chơi nha',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
